ace713e3d719a5427995adba49d4f02b51374e9b
findcod <fuzzyy@foxmail.com>
2017-06-23 23:34:27 +0800

    Update asyn.php

--- asyn.php
+++ asyn.php
@@ -70,3 +70,18 @@ function triggerRequest($url, $post_data = array(), $cookie = array()){
         sleep(3);
         file_put_contents(RUNTIME_PATH."/ceshi".time().".txt","ceshi");
     }
+
+function send_post($url, $post_data) {
+    $postdata = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            'header' => 'Content-type:application/x-www-form-urlencoded',
+            'content' => $postdata,
+            'timeout' => 0,// 超时时间（单位:s）
+            'Connection'=>'close'
+        )
+    );
+    $context = stream_context_create($options);
+    @file_get_contents($url, false, $context);
+}
